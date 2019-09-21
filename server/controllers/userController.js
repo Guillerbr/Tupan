c672e2e6744e7ae3d17ad981521f05e167562026
Guillerbr <ghminvest@gmail.com>
2019-09-21 17:04:24 -0300

    grantAccess executes permission if user has authorization

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -116,3 +116,21 @@ exports.deleteUser = async (req, res, next) => {
         next(error)
     }
 }
+
+
+//grantAccess executes permission if user has authorization         IMPORTANT
+exports.grantAccess = function(action, resource) {
+    return async (req, res, next) => {
+     try {
+      const permission = roles.can(req.user.role)[action](resource);
+      if (!permission.granted) {
+       return res.status(401).json({
+        error: "You don't have enough permission to perform this action"
+       });
+      }
+      next()
+     } catch (error) {
+      next(error)
+     }
+    }
+   }
