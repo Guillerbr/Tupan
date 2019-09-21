8c4d219d83591cc38b4cb19da4b0ddf999d52734
Guillerbr <ghminvest@gmail.com>
2019-09-21 17:12:27 -0300

    define default type headers server init file

--- server/server.js
+++ server/server.js
@@ -27,3 +27,23 @@ mongoose
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//define default type headers 
+app.use(async (req, res, next) => {
+    if (req.headers["x-access-token"]) {
+     const accessToken = req.headers["x-access-token"];
+     const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
+     // Check if token has expired
+     if (exp < Date.now().valueOf() / 1000) { 
+      return res.status(401).json({ error: "JWT token has expired, please login to obtain a new one" });
+     } 
+     res.locals.loggedInUser = await User.findById(userId); next(); 
+    } else { 
+     next(); 
+    } 
+   });
+   
+   app.use('/', routes); app.listen(PORT, () => {
+     console.log('Server is listening on Port:', PORT)
+   })
+
