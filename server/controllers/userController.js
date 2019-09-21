bdc5aa3092023a19e95c66394cd961b62e4daf0d
Guillerbr <ghminvest@gmail.com>
2019-09-21 16:49:55 -0300

    bcrypt functions auth and hash pass

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,4 +4,12 @@ const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-
+async function hashPassword(password) {             //functions bcrypt pass
+    return await bcrypt.hash(password, 10);
+   }
+   
+   async function validatePassword(plainPassword, hashedPassword) {    //bcrypt function auth
+    return await bcrypt.compare(plainPassword, hashedPassword);
+   }
+   
+  
