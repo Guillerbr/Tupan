8dec07a47ce698f468a87f456b7048e5aeecfb0a
Guillerbr <ghminvest@gmail.com>
2019-09-21 17:06:44 -0300

    allows access if user is logged in

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -134,3 +134,19 @@ exports.grantAccess = function(action, resource) {
      }
     }
    }
+
+
+   //allows access if user is logged in
+   exports.allowIfLoggedin = async (req, res, next) => {
+    try {
+     const user = res.locals.loggedInUser;
+     if (!user)
+      return res.status(401).json({
+       error: "You need to be logged in to access this route"
+      });
+      req.user = user;
+      next();
+     } catch (error) {
+      next(error);
+     }
+   }
