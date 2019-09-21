211e0e9562d7b2489ae3d9fc4c2d5024ac361116
Guillerbr <ghminvest@gmail.com>
2019-09-21 17:09:00 -0300

    server init dependences require

---
+++ server/server.js
@@ -0,0 +1,9 @@
+// server/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const path = require('path')
+const User = require('./models/userModel')
+const routes = require('./routes/route.js');
+
