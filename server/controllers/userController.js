ea4160d074531e0314942d6f0179676c30ca5336
Guillerbr <ghminvest@gmail.com>
2019-09-21 16:50:59 -0300

    function signup process post register

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,4 +12,23 @@ async function hashPassword(password) {             //functions bcrypt pass
     return await bcrypt.compare(plainPassword, hashedPassword);
    }
    
+   exports.signup = async (req, res, next) => {
+    try {
+     const { email, password, role } = req.body
+     const hashedPassword = await hashPassword(password);
+     const newUser = new User({ email, password: hashedPassword, role: role || "basic" });
+     const accessToken = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d"
+     });
+     newUser.accessToken = accessToken;
+     await newUser.save();
+     res.json({
+      data: newUser,
+      accessToken
+     })
+    } catch (error) {
+     next(error)
+    }
+   }
+
   
