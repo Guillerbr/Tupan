ecc4cc51ebe65d0e88e8cd0ffa86c7a93c385a4a
Guillerbr <ghminvest@gmail.com>
2019-09-21 16:53:21 -0300

    function login create post auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,31 +4,54 @@ const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-async function hashPassword(password) {             //functions bcrypt pass
+//functions bcrypt pass
+async function hashPassword(password) {
     return await bcrypt.hash(password, 10);
-   }
-   
-   async function validatePassword(plainPassword, hashedPassword) {    //bcrypt function auth
-    return await bcrypt.compare(plainPassword, hashedPassword);
-   }
-   
-   exports.signup = async (req, res, next) => {
-    try {
-     const { email, password, role } = req.body
-     const hashedPassword = await hashPassword(password);
-     const newUser = new User({ email, password: hashedPassword, role: role || "basic" });
-     const accessToken = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: "1d"
-     });
-     newUser.accessToken = accessToken;
-     await newUser.save();
-     res.json({
-      data: newUser,
-      accessToken
-     })
-    } catch (error) {
-     next(error)
-    }
-   }
+}
 
-  
+//bcrypt function auth
+async function validatePassword(plainPassword, hashedPassword) {
+    return await bcrypt.compare(plainPassword, hashedPassword);
+}
+
+//function regiter logic
+exports.signup = async (req, res, next) => {
+    try {
+        const { email, password, role } = req.body
+        const hashedPassword = await hashPassword(password);
+        const newUser = new User({ email, password: hashedPassword, role: role || "basic" });
+        const accessToken = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+            expiresIn: "1d"
+        });
+        newUser.accessToken = accessToken;
+        await newUser.save();
+        res.json({
+            data: newUser,
+            accessToken
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+//function login logic 
+exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) return next(new Error('Email does not exist'));
+        const validPassword = await validatePassword(password, user.password);
+        if (!validPassword) return next(new Error('Password is not correct'))
+        const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "1d"
+        });
+        await User.findByIdAndUpdate(user._id, { accessToken })
+        res.status(200).json({
+            data: { email: user.email, role: user.role },
+            accessToken
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
