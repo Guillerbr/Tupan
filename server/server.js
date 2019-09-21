a33ad8687654fe85cd43c1dcd0635e7a102f1cbb
Guillerbr <ghminvest@gmail.com>
2019-09-21 17:11:11 -0300

    server .env express PORT mongoose and body-parser configs

--- server/server.js
+++ server/server.js
@@ -7,3 +7,23 @@ const path = require('path')
 const User = require('./models/userModel')
 const routes = require('./routes/route.js');
 
+
+require("dotenv").config({
+    path: path.join(__dirname, "../.env")
+});
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+
+mongoose
+    .connect('mongodb://localhost:27017/rbac')
+    .then(() => {
+        console.log('Connected to the Database successfully');
+    });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
