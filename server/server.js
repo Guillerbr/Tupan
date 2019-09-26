d33410539d91d523f41b895324baada418a57cfc
Guillerbr <ghminvest@gmail.com>
2019-09-25 23:38:12 -0300

    authentication token response expired

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use(async (req, res, next) => {
      const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
      // Check if token has expired
      if (exp < Date.now().valueOf() / 1000) { 
-      return res.status(401).json({ error: "JWT token has expired, please login to obtain a new one" });
+      return res.status(401).send({ error: "JWT token has expired, please login to obtain a new one" });
      } 
      res.locals.loggedInUser = await User.findById(userId); next(); 
     } else { 
