a289eb7fa633e647242ee8c277879d7511268d6c
Guillerbr <ghminvest@gmail.com>
2020-04-20 10:54:56 -0300

    RPC module

--- server/api-bitcoin-core/api.js
+++ server/api-bitcoin-core/api.js
@@ -10,7 +10,7 @@ dotenv.config();
 const USER = process.env.RPC_USER;
 const PASS = process.env.RPC_PASSWORD;
 
-// const IP = process.env.RPC_IP
+// const IP = process.env.RPC_IP;
 
 /*
 const headers = {
