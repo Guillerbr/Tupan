9d126479644271ff7859fd4178c113d73a1fed23
guillerbr <guillerbrasilrj@gmail.com>
2020-07-19 20:08:31 -0300

    payments gateways MP

--- server/controllers/paympController.js
+++ server/controllers/paympController.js
@@ -18,39 +18,70 @@ exports.mpPayment = async (req, res, next) => {
   //  const { CardNumber, ExpirationDate, SecurityCode } = req.body
 
   var data = {
-    MerchantOrderId: "2014111703",
-    Customer: {
-      Name: "Comprador crédito simples"
+    token: "b3a7dbec3eb0d71798c4f19fec445795",
+    installments: 1,
+    transaction_amount: 58.8,
+    description:
+      "Point Mini a maquininha que dá o dinheiro de suas vendas na hora",
+    payment_method_id: "visa",
+    payer: {
+      email: "test_user_123456@testuser.com",
+      identification: {
+        number: "19119119100",
+        type: "CPF"
+      }
     },
-    Payment: {
-      Type: "CreditCard",
-      Amount: 15700,
-      Installments: 1,
-      SoftDescriptor: "123456789ABCD",
-      CreditCard: {
-        CardNumber: "1234123412341231",
-        Holder: "Teste Holder",
-        ExpirationDate: "12/2030",
-        SecurityCode: "123",
-        Brand: "Visa",
-        CardOnFile: {
-          Usage: "Used",
-          Reason: "Unscheduled"
+    notification_url: "https://www.suaurl.com/notificacoes/",
+    sponsor_id: null,
+    binary_mode: false,
+    external_reference: "MP0001",
+    statement_descriptor: "MercadoPago",
+    additional_info: {
+      items: [
+        {
+          id: "PR0001",
+          title: "Point Mini",
+          description:
+            "Producto Point para cobros con tarjetas mediante bluetooth",
+          picture_url:
+            "https://http2.mlstatic.com/resources/frontend/statics/growth-sellers-landings/device-mlb-point-i_medium@2x.png",
+          category_id: "electronics",
+          quantity: 1,
+          unit_price: 58.8
+        }
+      ],
+      payer: {
+        first_name: "Nome",
+        last_name: "Sobrenome",
+        address: {
+          zip_code: "06233-200",
+          street_name: "Av das Nacoes Unidas",
+          street_number: 3003
+        },
+        registration_date: "2019-01-01T12:01:01.000-03:00",
+        phone: {
+          area_code: "011",
+          number: "987654321"
         }
       },
-      IsCryptoCurrencyNegotiation: true
+      shipments: {
+        receiver_address: {
+          street_name: "Av das Nacoes Unidas",
+          street_number: 3003,
+          zip_code: "06233200",
+          city_name: "Buzios",
+          state_name: "Rio de Janeiro"
+        }
+      }
     }
   };
 
   axios
     .post(url, data, {
       headers: {
+       
         "Content-Type": "application/json",
-        MerchantId: "b17ac0ba-ff14-408a-93d7-dbcba07363b0",
-        MerchantKey: "XKSPPVZAZGAXATFPYBLNLKDHMLDMUENYIYJJXJUC"
-
-        //  "MerchantId": process.env.MERCHANTID,
-        //  "MerchantKey": process.env.MERCHANTKEY
+       
       }
     })
     .then(data => {
