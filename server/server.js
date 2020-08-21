5eac94d3ae0cbad881ee7b26f1b3d2ccb13cf2df
Guillerbr <guillerbrasilrj@gmail.com>
2020-08-21 00:28:03 -0300

    update libs

--- server/server.js
+++ server/server.js
@@ -65,7 +65,6 @@ app.use(bodyParser.urlencoded({ extended: false })).use(cors());
 //FEATURE COOKIE PARSER
 //app.use(cookieParser());
 
-
 //ACL RBAC TOKEN JWT-BEARER AUTHORIZATION IMPLEMENT FEATURE
 app.use(async (req, res, next) => {
   try {
